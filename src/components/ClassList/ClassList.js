bba10d56b118681dd7601631cee5be31a955b0c7
Alan Whitman <alanwhitman@Alans-MacBook-Air.local>
2018-10-30 13:55:31 -0600

    committing for submission

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default class ClassList extends Component {
   constructor() {
     super()
+    this.state = {
+      students: []
+    }
     
   }
-
+  componentDidMount() {
+    let nameOfClass = this.props.match.params.class
+    axios.get('http://localhost:3005/students?class=' + nameOfClass).then(res =>  {
+      this.setState({students: res.data});
+    }).catch(err => console.error(err));
+  }
   render() {
     return (
       <div className="box">
-        <h1></h1>
-        <h2>ClassList:</h2>
-
+        
+        <h2>ClassList: {this.props.match.params.class}</h2>
+        {this.state.students.map((student, i) =>  {
+          return(
+            <Link key={i} to={'/student/' + student.id}><h3>{student.first_name} {student.last_name}</h3></Link>
+          )
+        })}
       </div>
     )
   }
--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default class Student extends Component {
   constructor() {
     super()
-
+    this.state = {
+      studentInfo: {}
+    }
   }
-
+  componentDidMount() {
+    axios.get('http://localhost:3005/students/' + this.props.match.params.id).then(res => {
+      this.setState({studentInfo: res.data})
+    }).catch(err => console.error(err));
+  }
   render() {
     return (
       <div className="box">
         <h1>Student</h1>
+        <h1>{this.state.studentInfo.first_name} {this.state.studentInfo.last_name}</h1>
+        <p>Grade: {this.state.studentInfo.grade}<br />
+        Email: {this.state.studentInfo.email}</p>
+        <Link to={'/classlist/' + this.state.studentInfo.class}>Back</Link>
       </div>
     )
   }
